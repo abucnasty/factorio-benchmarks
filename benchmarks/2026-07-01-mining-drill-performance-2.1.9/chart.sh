cdb16e4faf96beffa74f96be97dd8bb57a15c34e
abucnasty <abucnasty@gmail.com>
2026-07-01 21:52:18 -0400

    results captured and charts created

---
+++ benchmarks/2026-07-01-mining-drill-performance-2.1.9/chart.sh
@@ -0,0 +1,72 @@
+# initial ticks to remove
+ticks_trim="60"
+# total number of ticks for timeseries charts
+ticks_total="1260"
+
+
+belt-charts boxplot "results/bm_*.csv" \
+    -w 1000 \
+    -h 800 \
+    --remove-first-ticks $ticks_trim \
+    --trim-prefix "bm_" \
+    -o "charts/run_distribution.png" \
+    --min-update 0
+
+belt-charts summary "results/bm_*.csv" \
+    -w 1000 \
+    -h 700 \
+    --remove-first-ticks $ticks_trim \
+    -o "charts/metrics.png" \
+    --aggregate-strategy "average" \
+    --metrics "wholeUpdate,controlBehaviorUpdate,transportLinesUpdate,electricHeatFluidCircuitUpdate,entityUpdate" \
+    --trim-prefix "bm_" \
+    --title-override "Mining Drill Metrics (1260 Ticks, 6 Runs)" \
+    --summary-table true
+
+belt-charts summary "results/bm_2_0_77_*.csv" \
+    -w 1000 \
+    -h 700 \
+    --remove-first-ticks $ticks_trim \
+    -o "charts/metrics_2_0_77.png" \
+    --aggregate-strategy "average" \
+    --metrics "wholeUpdate,controlBehaviorUpdate,transportLinesUpdate,electricHeatFluidCircuitUpdate,entityUpdate" \
+    --trim-prefix "bm_2_0_77_" \
+    --title-override "Mining Drill Metrics in 2.0.77 (1260 Ticks, 6 Runs)" \
+    --summary-table true \
+    --title-case
+
+belt-charts summary "results/bm_2_1_9_*.csv" \
+    -w 1000 \
+    -h 700 \
+    --remove-first-ticks $ticks_trim \
+    -o "charts/metrics_2_1_9.png" \
+    --aggregate-strategy "average" \
+    --metrics "wholeUpdate,controlBehaviorUpdate,transportLinesUpdate,electricHeatFluidCircuitUpdate,entityUpdate" \
+    --trim-prefix "bm_2_1_9_" \
+    --title-override "Mining Drill Metrics in 2.1.9 (1260 Ticks, 6 Runs)" \
+    --summary-table true \
+    --title-case
+
+
+belt-charts summary "results/bm_*4000*.csv" \
+    -w 1000 \
+    -h 700 \
+    --remove-first-ticks $ticks_trim \
+    -o "charts/metrics_mining_prod_4000.png" \
+    --aggregate-strategy "average" \
+    --metrics "wholeUpdate,controlBehaviorUpdate,transportLinesUpdate,electricHeatFluidCircuitUpdate,entityUpdate" \
+    --trim-prefix "bm_" \
+    --title-override "Mining Drill Metrics at Mining Prod 4000 (1260 Ticks, 6 Runs)" \
+    --summary-table false \
+    --title-case
+
+belt-charts bar "results/bm_*.csv" \
+    -w 1000 \
+    -h 800 \
+    --remove-first-ticks $ticks_trim \
+    -o "charts/timeseries.png" \
+    -a "average" \
+    --max-ticks $ticks_total \
+    --max-update 8000 \
+    --trim-prefix "bm_" \
+    --tick-window-aggregation 1
